a5fdf2e0e91e7683f25128158fe6f122022fc8b1
creativethumbs <ticha.seth@gmail.com>
2016-03-31 22:02:21 -0400

    translated score into arduinospeak

--- testino/testino.ino
+++ testino/testino.ino
@@ -7,25 +7,25 @@
 #include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
 #include <elapsedMillis.h>
 
-#define D_FLAT  2
-#define E_FLAT  3
-#define E_LOW   4
-#define F_LOW   5
-#define G_LOW   6
-#define A_FLAT  7
-#define A_LOW   8
-#define B_FLAT  9
-#define B_HIGH  10
-#define E_HIGH  11
-#define EB_BLOCK  0
-#define REST    -1
+#define D_FLAT     2
+#define E_FLAT     3
+#define E_LOW      4
+#define F_LOW      5
+#define G_LOW      6
+#define A_FLAT     7
+#define A_LOW      8
+#define B_FLAT     9
+#define B_HIGH    10
+#define E_HIGH    11
+#define EB_BLOCK   0
+#define REST      -1
 
 int ledPin = 13;
 
-int ScoreLength = 70;
+int ScoreLength = 93;
 int ScorePosition = 0;
 
-int Score [70][2] = { {B_HIGH, 1000}, {E_LOW, 1000},
+int Score [93][2] = { {B_HIGH, 1000}, {E_LOW, 1000},
   {A_LOW, 1000}, {REST, 500},  {B_HIGH, 125}, {E_HIGH, 375},
   {B_HIGH, 125}, {E_LOW, 250},  {REST, 125}, {B_HIGH, 1000}, {E_LOW, 1000},
   {A_LOW, 1000}, {REST, 500},
@@ -48,7 +48,15 @@ int Score [70][2] = { {B_HIGH, 1000}, {E_LOW, 1000},
   {E_FLAT, 375}, {F_LOW, 125},  {G_LOW, 250}, {REST, 250}, {D_FLAT, 2000},
   {REST, 1000},
   {EB_BLOCK, 1000}, {EB_BLOCK, 1000},
-  {A_LOW, 1000}, {REST, 500},  {B_HIGH, 125}, {E_FLAT, 375}
+  {A_LOW, 1000}, {REST, 500},  {B_HIGH, 125}, {E_HIGH, 375},
+  {B_HIGH, 125}, {E_LOW, 375}, {REST, 500}, {EB_BLOCK, 1000},
+  {EB_BLOCK, 1000}, {A_LOW, 2000},
+  {REST, 1000},
+  {REST, 250}, {B_FLAT, 250},  {A_FLAT, 500}, {G_LOW, 1000}, 
+  {G_LOW, 375}, {A_FLAT, 125},  {B_FLAT, 250}, {REST, 250}, {F_LOW, 2000},
+  {REST, 1000},
+  {E_FLAT, 375}, {F_LOW, 125},  {G_LOW, 250}, {REST, 250}, {D_FLAT, 2000},
+  {E_FLAT, 500}
 };
 
 void setup() {
